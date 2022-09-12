8014cad473149da3507f56c74f873d97973d5ca4
Dominykas <dominykasdrevinskas@gmail.com>
2022-09-12 10:16:31 +0300

    Updated table names, inserted new values, created new tables, updated getFoodData

--- createDatabase.php
+++ createDatabase.php
@@ -1,5 +1,5 @@
 <?php
-// Creates Database ant table Users and adds some data to it
+// Creates Database, table Users, Product and adds some data to it
 
 
 $servername = "localhost";
@@ -15,15 +15,15 @@ if ($conn->connect_error) {
 }
 
 // Create database
-$sql = "CREATE DATABASE Arena";
+$sql = "CREATE DATABASE IF NOT EXISTS Arena";
 if ($conn->query($sql) === TRUE) {
-    echo "Database created successfully";
+    // echo "Database created successfully<br>";
 } else {
     echo "Error creating database: " . $conn->error;
 }
 
 // sql to create table users
-$sql = "CREATE TABLE Arena.Users (
+$sql = "CREATE TABLE IF NOT EXISTS Arena.Users (
 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 email VARCHAR(50) NOT NULL UNIQUE,
 phoneNumber VARCHAR(50) NOT NULL UNIQUE,
@@ -32,52 +32,74 @@ password VARCHAR(50) NOT NULL
 
 
 if ($conn->query($sql) === TRUE) {
-    echo "Table Users created successfully";
+    echo "Table Users created successfully<br>";
 } else {
-    echo "Error creating table: " . $conn->error;
+    echo "Error creating table: " . $conn->error . "<br>";
 }
 
 // Insert some data into table
 $sql = "INSERT INTO Arena.Users (email, phoneNumber, password) 
 VALUES ('domke@arena.com', '+37069889781', 'domke'),
         ('user123@arena.com', '+37069889541', 'user123'),
-        ('jonas@arena.com', '+37069889542', 'jonas')";
+        ('jonas@arena.com', '+37069889542', 'jonas'),
+        ('test@arena.com', '+37069889543', 'test')";
 
 
 if ($conn->multi_query($sql) === TRUE) {
     echo "New records created successfully";
   } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Error: " . $sql . "<br>" . $conn->error . "<br>";
   }
 
 
-// sql to create table foodModel
-$sql = "CREATE TABLE Arena.FoodModel (
+// sql to create table product
+$sql = "CREATE TABLE Arena.product (
 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(50) NOT NULL,
 description VARCHAR(50) NOT NULL UNIQUE,
-price INT NOT NULL,
-favorite boolean NOT NULL
+type VARCHAR(50) NOT NULL,
+price FLOAT NOT NULL
 )";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Table FoodModel created successfully";
+    echo "Table product created successfully<br>";
 } else {
     echo "Error creating table: " . $conn->error;
 }
 
-// Insert some data into table
-$sql = "INSERT INTO Arena.FoodModel (name, description, price, favorite) 
-VALUES ('Pizza', 'Pizza with cheese', '10', '0'),
-        ('Pasta', 'Pasta with meat', '15', '0'),
-        ('Salad', 'Salad with vegetables', '5', '0'),
-        ('Burger', 'Burger with meat', '10', '0'),
-        ('Sandwich', 'Sandwich with vegetables', '5', '0')";
+// Insert some data into table product
+$sql = "INSERT INTO Arena.product (name, description, type, price) 
+VALUES ('Pica', 'Pica su sūriu', 'picos', '10'),
+        ('Pica aštri', 'Aštri pica', 'picos', '15'),
+        ('Mėsainis', 'Mėsainis su jautiena', 'mėsainiai', '5'),
+        ('Mėsainis', 'Mėsainis su vištiena', 'mėsainiai', '5'),
+        ('Ledai', 'Ledai su braškėmis', 'desertas', '3'),
+        ('Salotos skanios', 'Įvairios salotos', 'salotos', '2'),
+        ('Coca cola', 'Cola gėrimas', 'gėrimai', '1.5'),
+        ('Sprite', 'Sprite gėrimas', 'gėrimai', '1.5'),
+        ('Kepta duona', 'Kepta duona su sūriu', 'užkandžiai', '2'),
+        ('Kebabas su vištiena', 'Kebabas su vištiena', 'kebabai', '5')";
 
 if ($conn->multi_query($sql) === TRUE) {
     echo "New records created successfully";
   } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
   }
+
+// create table cart
+$sql = "CREATE TABLE Arena.cart (
+id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+user_id INT UNSIGNED NOT NULL,
+product_id INT UNSIGNED NOT NULL,
+quantity INT NOT NULL,
+FOREIGN KEY (user_id) REFERENCES Arena.Users(id),
+FOREIGN KEY (product_id) REFERENCES Arena.product(id)
+)";
+
+if($conn->query($sql) === TRUE){
+    echo "Table cart created successfully<br>";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
 
 $conn->close();
--- getFoodData.php
+++ getFoodData.php
@@ -6,7 +6,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM Arena.foodModel";
+$sql = "SELECT * FROM Arena.product";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     $data = array();
